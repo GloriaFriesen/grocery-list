ad5511baa72b44912e1b136adf827410ee1188d7
Gloria Friesen and Shane Stafford <shane@>
2017-02-20 16:45:49 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
   $("#listForm").submit(function(event) {
     event.preventDefault();
-      var items = ["item1", "item2", "item3"];
-      items.forEach(function(item) {
-        var outputs = $("input#" + item).val();
-        var outputsCaps = outputs.map(function(output){
-          return output.toUpperCase();
-        });
-        $("#listOutput").append("<li>" + outputsCaps + "</li>");
+      var items = ["#item1", "#item2", "#item3"];
+      var items2 = []
+      items.map(function(item) {
+        var outputs = $("input" + item).val();
+        items2.push(outputs);
       });
+      items2.sort();
+      var items3 = items2.map(function(x) {
+        return x.toUpperCase();
+      });
+      items3.forEach(function(y){
+      $("#listOutput").append("<li>" + y + "</li>");
     });
   });
+});
